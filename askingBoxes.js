e26ca97dfade43f849356668a7a1759554a60fba
Literal Line <13988479+shelbywallie2@users.noreply.github.com>
2020-06-18 22:38:57 -0400

    create

--- askingBoxes.js
+++ askingBoxes.js
@@ -8,6 +8,8 @@ var answer = document.getElementById('answer').style;
 
 console.log('askingBoxes.js loaded!');
 console.log('v1.0 by Literal Line');
+console.log('');
+console.log('Remember to remove the asking-box, question, and answer styles specified in the HTML file!');
 
 
 // setup new box appearance, supports specific locations, etc.
@@ -16,16 +18,29 @@ function initBox() {
     askingBox.top = '50%';
     askingBox.left = '50%';
     askingBox.transform = 'translate(-50%, -50%)';
+    askingBox.minWidth = '400px';
     askingBox.border = '1px solid #000000';
     askingBox.borderRadius = '3px';
     askingBox.padding = '5px';
+    askingBox.backgroundColor = 'rgba(255, 255, 255, 0.75';
+    askingBox.fontFamily = 'sans-serif';
+    askingBox.fontSize = '16pt';
 
-    question.padding = '5px';
-    
+    question.minWidth = '90%';
+    question.margin = '10px';
+    question.marginTop = '55px';
+    question.whiteSpace = 'nowrap';
+
+    answer.minWidth = '90%';
+    answer.margin = '10px';
+    answer.padding = '3px';
 }
 
-
-
+// box location function
+function boxLocation(leftpx, toppx) {
+    askingBox.left = String(leftpx) + '%';
+    askingBox.top = String(toppx) + '%';
+}
 
 
 initBox();
